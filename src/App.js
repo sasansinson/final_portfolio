7e38003ef42bec347d3cfb4a1d1185339640b2e4
sasan sahami <sasan_2002@yahoo.com>
2022-05-07 13:29:15 +1000

    add more files and update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import Portfolio from "./components/pages/Portfolio";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
+import HeroImage from "./components/HeroImage";
+import FooterInfo from "./components/FooterInfo"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={ process.env.PUBLIC_URL }>
+      <div>
+        <HeroImage/>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Portfolio" component={Portfolio} />
+        <Route exact path="/Projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <FooterInfo/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/FooterInfo.js
@@ -0,0 +1,19 @@
+//footer will go here
+import "./styles.css";
+import React from "react";
+
+function FooterInfo() {
+    return (
+    <div>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+    <div className="bg-dark text-white fixed-bottom footer mt-auto py-3">
+    <span className="text-muted">Designed and Developed by Sasan Sahami &bull; Copyright © 2022 SS</span>
+    </div>
+    </div>
+    );
+}
+
+export default FooterInfo;
