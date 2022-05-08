c0ef6de6e987a4007d88c6c6c2f1fc02324924b7
sasan sahami <sasan_2002@yahoo.com>
2022-05-08 17:41:20 +1000

    Updates

---
+++ precache-manifest.9a3e2c7225171216a517f37fab8f43b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "221e95095650e5c03c7d",
+    "url": "/final_portfolio/static/css/main.4c24acf2.chunk.css"
+  },
+  {
+    "revision": "221e95095650e5c03c7d",
+    "url": "/final_portfolio/static/js/main.34ee9693.chunk.js"
+  },
+  {
+    "revision": "dedcc21f121515078850",
+    "url": "/final_portfolio/static/js/runtime~main.e3373eca.js"
+  },
+  {
+    "revision": "aaa80a1a5aadfc800116",
+    "url": "/final_portfolio/static/css/2.cb897cf1.chunk.css"
+  },
+  {
+    "revision": "aaa80a1a5aadfc800116",
+    "url": "/final_portfolio/static/js/2.61a56fee.chunk.js"
+  },
+  {
+    "revision": "cd52c10298176281e453610b98612d0a",
+    "url": "/final_portfolio/static/media/profile_pic.cd52c102.png"
+  },
+  {
+    "revision": "aa5942c2e2a28ae95a308a980b0f14f2",
+    "url": "/final_portfolio/static/media/PDF.aa5942c2.png"
+  },
+  {
+    "revision": "a574681fa3c59fc628356e4729db4579",
+    "url": "/final_portfolio/static/media/Sasan_Sahamimoghaddam_Resume.a574681f.PDF"
+  },
+  {
+    "revision": "294a3913132ddae08bb9708cf227ca0f",
+    "url": "/final_portfolio/static/media/sasan.294a3913.png"
+  },
+  {
+    "revision": "977cda7c6df5d0f068b538a7a310549a",
+    "url": "/final_portfolio/index.html"
+  }
+];
