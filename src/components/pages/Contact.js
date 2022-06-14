a0cd3c2a2823155d540a163d2e1651ef8eaeb00d
sasan sahami <sasan_2002@yahoo.com>
2022-06-14 20:15:40 +1000

    new resume added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,7 @@ import github_icon from "../images/github_logo.png";
 import linkedIn_icon from "../images/linkedin.png";
 import email_icon from "../images/email_icon.png";
 import pdfIcon from "../images/PDF.png";
-import Resume from "../images/Sasan_Sahamimoghaddam_Resume.PDF";
+import Resume from "../images/Sasan_Sahami_Resume.PDF";
 
 function Contact(props) {
   return (
@@ -13,7 +13,7 @@ function Contact(props) {
    <p><img className="contactImg" src={ pdfIcon } alt="github icon" /><a className="contactLink" href={Resume}>Click here to download my resume</a></p>
     <img className="contactImg" src={ github_icon } alt="github icon" /><a className="contactLink" href="https://github.com/sasansinson" target="_blank" rel="noreferrer"> Github</a><br/>
     <img className="contactImg" src={ linkedIn_icon } alt="linkedIn icon" /><a className="contactLink" href="https://www.linkedin.com/in/sasan-sahami-71aa017b/" target="_blank" rel="noreferrer"> LinkedIn</a><br/>
-    <img className="contactImg" src={ email_icon } alt="email icon" /><a className="contactLink" href="mailto:sasan_2002@yahoo.com">Email me</a>
+    <img className="contactImg" src={ email_icon } alt="email icon" /><a className="contactLink" href="mailto:sas.sahami@gmail.com">Email me</a>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
